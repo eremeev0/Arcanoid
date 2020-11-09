780d98502a431f212fe79154fbc2961814ad1b3c
eremeev0 <m_eremeev@mifprojects.com>
2020-11-09 14:34:09 +0200

    rollback project, fixed bugs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,13 +18,16 @@ namespace Assets.Scripts
 
         void OnTriggerEnter2D(Collider2D col)
         {
-            msgManager.OnTriggerEnter2D(col);
+            if (col.name == "TerritoryLimiter")
+                transform.position = new Vector3(transform.position.x, transform.position.y - .2f, -1);
+            if (col.name == "GameOver")
+                transform.position = new Vector3(transform.position.x, transform.position.y + .2f, -1);
         }
 
         // Update is called once per frame
         void Update()
         {
-            transform.position = platform.GetPosition();
+            //transform.position = platform.GetPosition();
             Speed = platform.GetPlayerSpeed(); 
             gameObject.GetComponent<SpriteRenderer>().color = platform.GetPlayerColor();
             var transformRotation = transform.rotation;
