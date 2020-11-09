a5376495765ad0b16a44eb6dceaa3c862f01b1b6
eremeev0 <m_eremeev@mifprojects.com>
2020-11-09 18:13:39 +0200

    created empty class GameLogic

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GameLogic: MonoBehaviour
+    {
+        private GameObject go;
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            go = GameObject.Find("/ActiveObjects/Platforms/Platform");
+            if (go == null)
+            { 
+                // player win
+            }
+        }
+    }
+}
