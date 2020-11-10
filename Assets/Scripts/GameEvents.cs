9e606219e85a9a0c6a6478490bf20718a1b86a01
eremeev0 <m_eremeev@mifprojects.com>
2020-11-10 14:22:12 +0200

    event types updated

--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -3,11 +3,15 @@
     public enum GameEvents
     {
         GAME_STARTED,
-        GAME_EXITED,
+        GAME_CLOSED,
         GAME_RESTARTED,
         GAME_BACK_TO_MENU,
-        POINTS_UPDATED,
+        GAME_BACK_TO_MENU_2,
+        GAME_OPEN_OPTIONS,
+        WIN_RESOLUTION_UPDATED,
+        SCORE_UPDATED,
         SPEED_UPDATED,
+        PLAYER_COLOR_UPDATED,
         LEVEL_FAILED,
     }
 }
