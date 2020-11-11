37ed02c7d3657ca7fe8a536cd142d8bc683e9cc0
eremeev0 <m_eremeev@mifprojects.com>
2020-11-11 13:46:41 +0200

    stop and resume logic updated

---
+++ Assets/Scripts/ConfigurationManagment/Settings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.ConfigurationManagment
+{
+    /// <summary>
+    /// this class contain all game properties
+    /// </summary>
+    public class Settings
+    {
+        public static float PlayerSpeed { get; set; } = 5f;
+        public static Color PlayerColor { get; set; } = Color.white;
+        public static int PlayerScore { get; set; } = 0;
+
+        public static bool IsGameStopped { get; set; } = false;
+    }
+}
