c93d9774a5b0551777b8957529ef7bae0ca82d8d
eremeev0 <m_eremeev@mifprojects.com>
2020-11-12 16:56:24 +0200

    removed unused member

--- Assets/Scripts/Performances/Services/DestrPlatformService.cs
+++ Assets/Scripts/Performances/Services/DestrPlatformService.cs
@@ -6,34 +6,24 @@ namespace Assets.Scripts.Performances.Services
     public class DestrPlatformService: MonoBehaviour, IDestrPlatformService
     {
         private GameObject _list;
-        private int _destroyed = 0;
         private bool _isAllDestroyed;
         void Start()
         {
             _list = GameObject.Find("/ActiveObjects/Platforms");
-            print(_list.transform.childCount);
             _isAllDestroyed = false;
         }
 
         void Update()
         {
-            //print(_list.GetComponentsInChildren<Sprite>());
-            //print(_list);
+
         }
 
         public void Destroy()
         {
-            print(_list.transform.childCount);
             if (_list.transform.childCount == 1)
             {
                 _isAllDestroyed = true;
             }
-            /*Destroy(_destroyedPlatforms);
-            _destroyedPlatforms = GameObject.Find("/ActiveObjects/Platforms").GetComponentInChildren<Sprite>();
-            if (_destroyedPlatforms == null)
-            {
-                _isAllDestroyed = true;
-            }*/
         }
 
         public bool IsAllDestroyed()
