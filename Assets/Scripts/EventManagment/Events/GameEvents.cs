a699911eb25fc3531a563f4d41c13c1a99ab1921
eremeev0 <m_eremeev@mifprojects.com>
2020-11-12 16:51:09 +0200

    distribution of events by responsibility

--- Assets/Scripts/EventManagment/Events/GameEvents.cs
+++ Assets/Scripts/EventManagment/Events/GameEvents.cs
@@ -2,19 +2,8 @@
 {
     public enum GameEvents
     {
-        GAME_STARTED,
-        GAME_CLOSED,
-        GAME_RESTARTED,
-        GAME_BACK_TO_MENU,
-        GAME_OPEN_OPTIONS,
         GAME_PAUSED,
         GAME_RESUMED,
-        SAVE_OPTIONS,
-        RESET_OPTIONS,
-        WIN_RESOLUTION_UPDATED,
-        SCORE_UPDATED,
-        SPEED_UPDATED,
-        PLAYER_COLOR_UPDATED,
         LEVEL_FAILED,
     }
 }
--- Assets/Scripts/EventManagment/Events/UIEvents.cs
+++ Assets/Scripts/EventManagment/Events/UIEvents.cs
@@ -3,6 +3,17 @@
     public enum UIEvents
     {
         INIT_COLOR_LIST,
-        INIT_RESOLUTION_LIST
+        INIT_RESOLUTION_LIST,
+        GAME_STARTED,
+        GAME_CLOSED,
+        GAME_RESTARTED,
+        GAME_BACK_TO_MENU,
+        GAME_OPEN_OPTIONS,
+        SAVE_OPTIONS,
+        RESET_OPTIONS,
+        WIN_RESOLUTION_UPDATED,
+        SCORE_UPDATED,
+        SPEED_UPDATED,
+        PLAYER_COLOR_UPDATED,
     }
 }
