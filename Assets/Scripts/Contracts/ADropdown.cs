4db01f7142494a6d55f95176c78e4ff78218a068
eremeev0 <m_eremeev@mifprojects.com>
2020-11-16 18:10:53 +0200

    added UI object templates

---
+++ Assets/Scripts/Contracts/ADropdown.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Contracts
+{
+    public class ADropdown
+    {
+        public Dropdown UIDropdown;
+
+        public ADropdown([NotNull] Dropdown uiDropdown)
+        {
+            UIDropdown = uiDropdown;
+        }
+
+        [CanBeNull]
+        public delegate void Onclick();
+    }
+}
---
+++ Assets/Scripts/Contracts/ASlider.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Contracts
+{
+    public class ASlider
+    {
+        public Slider UISlider;
+        
+        public Text ValueLabel;
+        public ASlider([NotNull]Slider uiSlider,[CanBeNull] Text valueLabel)
+        {
+            UISlider = uiSlider;
+            ValueLabel = valueLabel;
+        }
+        [CanBeNull]
+        public delegate void OnClick();
+    }
+}
