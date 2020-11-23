ebb2d1208705ffc2372d2f3695ed135508903c3c
eremeev0 <m_eremeev@mifprojects.com>
2020-11-23 18:53:37 +0200

    remove all saved data to json format

--- Assets/Scripts/Cross/StatesManagament/ActionsContainer/SettingsActionsContainer.cs
+++ Assets/Scripts/Cross/StatesManagament/ActionsContainer/SettingsActionsContainer.cs
@@ -16,10 +16,10 @@ namespace Assets.Scripts.Cross.StatesManagament.ActionsContainer
         public void SaveSettings()
         {
             _dataManager.Save(
-                $"[PlayerSpeed] = {JsonUtility.ToJson(SettingsSingleton.GetSettings().PlayerSpeed)}",
-                $"[PlayerColor] = {JsonUtility.ToJson(SettingsSingleton.GetSettings().PlayerColor)}",
-                $"[GameResolution] = {JsonUtility.ToJson(SettingsSingleton.GetSettings().GameResolution)}",
-                $"[PlayerScore] = {JsonUtility.ToJson(SettingsSingleton.GetSettings().PlayerScore)}"
+                JsonUtility.ToJson(SettingsSingleton.GetSettings().PlayerSpeed),
+                JsonUtility.ToJson(SettingsSingleton.GetSettings().PlayerColor),
+                JsonUtility.ToJson(SettingsSingleton.GetSettings().GameResolution),
+                JsonUtility.ToJson(SettingsSingleton.GetSettings().PlayerScore)
                 );
         }
 
@@ -28,32 +28,20 @@ namespace Assets.Scripts.Cross.StatesManagament.ActionsContainer
             var settings = _dataManager.Load();
             if (settings == null) 
                 return;
-            string value = string.Empty;
-            
-            value = $"[PlayerSpeed] = ";
-            SettingsSingleton.GetSettings().PlayerSpeed =
-                JsonUtility.FromJson<float>(settings[0].Remove(0, value.Length));
 
-            value = $"[PlayerColor] = ";
-            SettingsSingleton.GetSettings().PlayerColor =
-                JsonUtility.FromJson<Color>(settings[1].Remove(0, value.Length));
-
-            value = $"[GameResolution] = ";
-            SettingsSingleton.GetSettings().GameResolution =
-                JsonUtility.FromJson<Vector2>(settings[2].Remove(0, value.Length));
-            
-            value = $"[PlayerScore] = ";
-            SettingsSingleton.GetSettings().PlayerScore =
-                JsonUtility.FromJson<int>(settings[3].Remove(0, value.Length));
+            SettingsSingleton.GetSettings().PlayerSpeed = JsonUtility.FromJson<float>(settings[0]);
+            SettingsSingleton.GetSettings().PlayerColor = JsonUtility.FromJson<Color>(settings[1]);
+            SettingsSingleton.GetSettings().GameResolution = JsonUtility.FromJson<Vector2>(settings[2]);
+            SettingsSingleton.GetSettings().PlayerScore = JsonUtility.FromJson<int>(settings[3]);
         }
 
         public void ResetSettings()
         {
             _dataManager.Save(
-                $"[PlayerSpeed] = {JsonUtility.ToJson(DefaultSettingsDto.PlayerSpeed)}",
-                $"[PlayerColor] = {JsonUtility.ToJson(DefaultSettingsDto.PlayerColor)}",
-                $"[GameResolution] = {JsonUtility.ToJson(DefaultSettingsDto.GameResolution)}",
-                $"[PlayerScore] = {JsonUtility.ToJson(DefaultSettingsDto.PlayerScore)}"
+                JsonUtility.ToJson(DefaultSettingsDto.PlayerSpeed),
+                JsonUtility.ToJson(DefaultSettingsDto.PlayerColor),
+                JsonUtility.ToJson(DefaultSettingsDto.GameResolution),
+                JsonUtility.ToJson(DefaultSettingsDto.PlayerScore)
             );
         }
     }
