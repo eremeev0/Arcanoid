143908a8f3d06e02d2d89f964c8c99677cff72a8
eremeev0 <m_eremeev@mifprojects.com>
2020-11-25 12:04:46 +0200

    ball settings from SettingsSingleton has been moved to new class

---
+++ Assets/Scripts/MultiOriented/Contracts/BallSettingsDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Assets.Scripts.MultiOriented.Contracts
+{
+    public class BallSettingsDto
+    {
+        private BallSettingsDto(){}
+        private static BallSettingsDto _ballSettings;
+
+        public static BallSettingsDto GetBallSettings()
+        {
+            return _ballSettings ?? (_ballSettings = new BallSettingsDto());
+        }
+        public float BallSpeed { get; set; } = 1f;
+        public float BallMaxSpeed { get; set; } = 14f;
+        public float BallSpeedMultiplier { get; set; } = .1f;
+    }
+}
