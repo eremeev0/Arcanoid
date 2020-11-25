b844809c21aa5df1d6e717ac38a57ed738d6b237
eremeev0 <m_eremeev@mifprojects.com>
2020-11-25 11:54:52 +0200

    added a script that acts as a flag to validate the freezing method

---
+++ Assets/Scripts/MultiOriented/CheckComponent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.MultiOriented
+{
+    public class CheckComponent: MonoBehaviour
+    {
+        public bool IsFreeze;
+
+        void Start()
+        {
+
+        }
+    }
+}
