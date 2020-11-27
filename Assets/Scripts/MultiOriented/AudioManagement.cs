05a42e927f4edd1f58e15b4de954126171a3a5a1
eremeev0 <m_eremeev@mifprojects.com>
2020-11-27 17:39:02 +0200

    added audio manager (without implementation)

---
+++ Assets/Scripts/MultiOriented/AudioManagement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.MultiOriented
+{
+    public class AudioManagement
+    {
+        private AudioSource _audioSource;
+        private AudioClip _audioClip;
+        void SetAudioSource(AudioSource audioSource) { }
+        void SetAudioClip(AudioClip audioClip) { }
+        void SwitchClip(string clipName) { }
+        void UpdateVolume(int volume) { }
+        public override string ToString()
+        {
+            return $"(src: {_audioSource}, clip: {_audioClip})";
+        }
+    }
+}
