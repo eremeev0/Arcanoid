d90c3293dd513ebba4e3fa55404edc8c2d851d1e
eremeev0 <m_eremeev@mifprojects.com>
2020-12-01 14:38:56 +0200

    added a class responsible for the platform logic

---
+++ Assets/Scripts/GameScene/ConcretePlatform.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+namespace Assets.Scripts.GameScene
+{
+    [Serializable]
+    public class ConcretePlatform: MonoBehaviour
+    {
+        public Guid guid;
+        public Vector3 position;
+        public Color color;
+        public int health = 1;
+        public int points = 1;
+
+        public void Initialization(Guid guid, Color color)
+        {
+            this.guid = guid;
+            this.color = color;
+        }
+        public void Initialization(Guid guid, Color color, int health)
+        {
+            this.guid = guid;
+            this.color = color;
+            this.health = health;
+        }
+
+        //Unity Start Message
+        private void Start()
+        {
+            GetComponent<SpriteRenderer>().color = color;
+            position = transform.position;
+        }
+
+
+        public Guid GetGuid() { return guid;}
+        public int GetHealth() { return health;}
+        
+        //Unity Update Message
+        private void Update()
+        {
+
+        }
+
+    }
+}
